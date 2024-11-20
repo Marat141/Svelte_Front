8bae5166bac3cc6f3cb9cbc8725e835d3983f223
Marat141 <terest141@gmail.com>
2024-11-20 14:28:37 +0100

    15

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,29 @@
+<script>
+    // Získání atributů z `$props`, s výchozími hodnotami
+    let { href = "#", color = "default", children } = $props();
+
+    // Dynamická třída
+    let Button_class = `${color} button`;
+</script>
+
+<a {href} class={Button_class}>
+    {@render children()} <!-- Správné použití {@render} pro vykreslení children -->
+</a>
+
+<style lang="scss">
+    /* Stylování tlačítka */
+    .button {
+        padding: 10px 20px;
+        border-radius: 5px;
+        text-decoration: none;
+        display: inline-block;
+    }
+    .default {
+        background-color: #ccc;
+        color: #000;
+    }
+    .red {
+        background-color: #fc0000;
+        color: #ffffff;
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+import Button from '$lib/components/Button.svelte'
+</script>
+<h1>Welcome to SvelteKit</h1>
+<Button color="red">Login</Button>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
